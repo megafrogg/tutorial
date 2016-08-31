371378574b67b98072b622bdbe1bc12f91b42433
Mitchell Rathgeb <megafrogg@gmail.com>
2016-08-30 22:19:30 -0400

    First Commit

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Hello word
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<Main />, document.getElemementById('app'));
