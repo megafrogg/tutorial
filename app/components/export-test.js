7d38ad5fcd0f25cc2e2e5a45b97597421571e3bb
Mitchell Rathgeb <megafrogg@gmail.com>
2016-08-31 01:19:41 -0400

    Delete bundle.js

--- app/components/export-test.js
+++
@@ -1,8 +0,0 @@
-const someString = 'Hi everyone';
-const someObj = {first: 'Della', last: 'Ella'};
-
-const defaultThing = 'I AM DEFAAAAAAULT';
-
-
-export default defaultThing;
-export {someString, someObj};
